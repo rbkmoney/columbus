3e49a72e18d05d2f01f412c450c90ab51dfcfed5
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-06-26 18:28:53 +0300

    Test commit

--- src/test/java/com/rbkmoney/columbus/AbstractIntegrationTest.java
+++ src/test/java/com/rbkmoney/columbus/AbstractIntegrationTest.java
@@ -1,6 +1,12 @@
 package com.rbkmoney.columbus;
 
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.DockerPort;
+import com.palantir.docker.compose.connection.waiting.ClusterWait;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
 import com.rbkmoney.columbus.check.LogInterceptor;
+import com.rbkmoney.columbus.check.PostgresIsReadyCheck;
+import org.flywaydb.core.Flyway;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +17,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
-import java.io.File;
+import java.time.Duration;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -22,21 +30,27 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @ContextConfiguration(classes = ColumbusApplication.class, initializers = AbstractIntegrationTest.Initializer.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public abstract class AbstractIntegrationTest {
-    public  static LogInterceptor logInterceptor = new LogInterceptor();
 
     @ClassRule
-    public static DockerComposeContainer compose =
-            new DockerComposeContainer(
-                    new File("src/test/resources/docker-compose.yml"))
-                    .withExposedService("postgres", 5432);
+    public static PostgreSQLContainer postgres = (PostgreSQLContainer) new PostgreSQLContainer("dr2.rbkmoney.com/rbkmoney/postgres-geodata:0eb52256576ec22f89fadc3e7fe505b692e838a3")
+            .withStartupTimeout(Duration.ofMinutes(5));
 
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            String serviceHost = compose.getServiceHost("postgres", 5432);
-            TestPropertyValues
-                    .of("spring.datasource.url=" + "jdbc:postgresql://" + serviceHost + ":" + 5432 + "/postgres")
-                    .applyTo(configurableApplicationContext.getEnvironment());
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword(),
+                    "flyway.url=" + postgres.getJdbcUrl(),
+                    "flyway.user=" + postgres.getUsername(),
+                    "flyway.password=" + postgres.getPassword()
+            ).applyTo(configurableApplicationContext);
+            Flyway flyway = Flyway.configure()
+                    .dataSource(postgres.getJdbcUrl(), postgres.getUsername(), postgres.getPassword())
+                    .schemas("af")
+                    .load();
+            flyway.migrate();
         }
     }
 
