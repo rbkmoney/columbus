fb5fe317bb91d32c3c7f62081b7f46f34acf9a74
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-06-26 18:03:12 +0300

    Fix tests

--- src/test/java/com/rbkmoney/columbus/AbstractIntegrationTest.java
+++ src/test/java/com/rbkmoney/columbus/AbstractIntegrationTest.java
@@ -1,12 +1,6 @@
 package com.rbkmoney.columbus;
 
-import com.palantir.docker.compose.DockerComposeRule;
-import com.palantir.docker.compose.connection.DockerPort;
-import com.palantir.docker.compose.connection.waiting.ClusterWait;
-import com.palantir.docker.compose.connection.waiting.HealthChecks;
 import com.rbkmoney.columbus.check.LogInterceptor;
-import com.rbkmoney.columbus.check.PostgresIsReadyCheck;
-import org.joda.time.Duration;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,32 +10,32 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.DockerComposeContainer;
+
+import java.io.File;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ContextConfiguration(classes = ColumbusApplication.class, initializers = AbstractIntegrationTest.Initializer.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public abstract class AbstractIntegrationTest {
     public  static LogInterceptor logInterceptor = new LogInterceptor();
 
     @ClassRule
-    public static DockerComposeRule docker = DockerComposeRule.builder()
-            .file("src/test/resources/docker-compose.yml")
-            .logCollector(logInterceptor)
-            .addClusterWait(new ClusterWait(new PostgresIsReadyCheck(logInterceptor.getIn()), Duration.standardMinutes(2)))
-            .waitingForService("postgres", HealthChecks.toHaveAllPortsOpen())
-            .build();
+    public static DockerComposeContainer compose =
+            new DockerComposeContainer(
+                    new File("src/test/resources/docker-compose.yml"))
+                    .withExposedService("postgres", 5432);
 
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            DockerPort postgres = docker.containers().container("postgres").port(5432);
-            final String dbUrl = postgres.inFormat("jdbc:postgresql://$HOST:$EXTERNAL_PORT/postgres");
+            String serviceHost = compose.getServiceHost("postgres", 5432);
             TestPropertyValues
-                    .of("pring.datasource.url=" + dbUrl)
+                    .of("spring.datasource.url=" + "jdbc:postgresql://" + serviceHost + ":" + 5432 + "/postgres")
                     .applyTo(configurableApplicationContext.getEnvironment());
         }
     }
